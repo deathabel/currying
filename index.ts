22e188d6c88a43663584206ca77957652a1cac85
deathabel <deathabel@gmail.com>
2022-08-31 10:20:41 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -2,15 +2,22 @@
  *
  */
 const shoesPrices = [100, 80];
+const clothesPrices = [120, 200, 150];
+// 計算Function
 const totalPrice = (...prices) => prices.reduce((pre, cur) => pre + cur, 0);
 const discount = (price, off) => price * ((100 - off) / 100);
-console.log(discount(totalPrice(...shoesPrices), 10));
-
+// totalPrice Currying
 const totalPriceCurrying =
-  (startPrice) =>
+  () =>
   (...prices) =>
-    startPrice + totalPrice(...prices);
+    totalPrice(...prices);
+// dicount currying
 const discountCurrying = (off) => (price) => discount(price, off);
+/**
+ * calculate pipe
+ * 先將公式傳入不作計算，拋出組合後的計算公式 (currying)
+ * 計算時將每個公式的結果拋入給下一個公式去進行計算 (pipe)
+ */
 const calculate =
   (...formula) =>
   (...prices) =>
@@ -20,36 +27,25 @@ const calculate =
       prices
     );
 
+// 商品全面九折計算公式
 const totalPriceCalculate = calculate(
-  totalPriceCurrying(0),
+  totalPriceCurrying(),
   discountCurrying(10)
 );
-
+console.log('A顧客金額');
 console.log(totalPriceCalculate(...shoesPrices));
 
-const clothesPrices = [120, 200, 150];
-// different discount shoe 10, cloth 20 off
-// total discount 10 off
-console.log(
-  discount(
-    totalPrice(
-      discount(totalPrice(...shoesPrices), 10),
-      discount(totalPrice(...clothesPrices), 20)
-    ),
-    10
-  )
-);
-
+// 鞋子折扣計算公式
 const shoesPriceCalcuate = calculate(
-  totalPriceCurrying(0),
+  totalPriceCurrying(),
   discountCurrying(10)
 );
-
+// 衣服折扣計算公式
 const clothesPriceCalcuate = calculate(
-  totalPriceCurrying(0),
+  totalPriceCurrying(),
   discountCurrying(20)
 );
-
+console.log('B顧客金額');
 console.log(
   totalPriceCalculate(
     shoesPriceCalcuate(...shoesPrices),
